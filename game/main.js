e8744c49c76b9fee7c2cb604215d3f35c510178d
manatoueda <jnm-940-nts@e-net.nara.jp>
2025-09-22 06:22:04 +0000

    弾を打つ

--- game/main.js
+++ game/main.js
@@ -1,51 +1,67 @@
-document.getElementById("txt").innerHTML = "これはゲームです";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d")
-let x = 255;
-let y = 480;
-let w = 0;
-let a = 140;
-let b = 480;
+
+const player = {
+  x: canvas.clientWidth / 2 - 15,
+  y: canvas.clientHeight - 60,
+  width: 30,
+  height: 30,
+  color: "yellow",
+life: 3,
+};
+
+const bullets = [];
+const BULLET_SPEED = -10;
+function tryShoot() {
+bullets.push({
+    x: player.x,
+    y: player.y,
+    width: 5,
+    height: 5,
+    vy: BULLET_SPEED,
+  });
+}
 window.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft") {
-    x -= 10;
+    player.x -= 10;
   } else if (e.key === "ArrowRight") {
-    x += 10;
+    player.x += 10;
+  } else if (e.code === "Space") {
+    tryShoot();
   }
-})
-window.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowUp") {
-    a-= 10;
-  } else if (e.key === "ArrowDown") {
-    a += 10;
+});
+
+function update() {
+  for (let i = bullets.length - 1; i >= 0; i--) {
+    const bullet = bullets[i];
+    bullet.y += bullet.vy;
+    if (bullet.y < 0) {
+      bullets.splice(i, 1);
+    }
+}
+}
+function draw() {
+  ctx.fillStyle = "black";
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+  ctx.fillStyle = "white";
+  for (let i = 0; i < bullets.length; i++) {
+    const bullet = bullets[i];
+    ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
   }
-})
-window.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowAkey") {
-    b-= 10;
-  } else if (e.key === "ArrowD") {
-    b += 10;
-  }
-})
+
+}
+
+
+
 
 
 function gameLoop() {
-
-ctx.fillStyle = "black";
-ctx.fillRect (0,0,canvas.clientWidth, canvas.clientHeight);
-
-
-
-ctx.fillStyle = "blue";
-ctx.fillRect (x,y, 30, 30);
-
-ctx.fillStyle = "vaiolet";
-ctx.fillRect (160,w, 4, 60  );
-ctx.fillStyle = "red";
-ctx.fillRect (a,b, 4, 60  );
-w+= 5;
-
-
-requestAnimationFrame(gameLoop);
+update();
+draw();
+  requestAnimationFrame(gameLoop);
 }
+
 gameLoop();
